f95c90b48dbd813f5c1e29e9a8fa994805726727
Nazri Dwi Liansyah <nazri.dwiliansyah78@gmail.com>
2019-06-30 14:27:29 +0700

    part2

--- chapter-2/practice-3/index.js
+++ chapter-2/practice-3/index.js
@@ -17,3 +17,53 @@ const students=[{
 }]
 let gradeBelow55=students.filter(students => students.grade<55);
 console.log('Dibawah 55',gradeBelow55)
+
+// let gender="Male";
+// const newData = students.map(x = gender);//Mengubah item" ke array yang baru
+// console.log('new array are: ', newData);
+
+// let array1 = [1,4,9,16];
+// const newArray = array1.map(x => x *2);//Mengubah item" ke array yang baru
+// console.log('new array are: ', newArray);
+
+const object={
+    name: ' Budi',
+    gender:'Male',
+    live : 'Batam'
+}
+// console.log(object);
+
+// const{ name,live}= object;
+// console.log(name,live)
+
+// const simpleFunction = ({name,gender})=>`hi my name is ${name} with gender ${gender}`
+// const greeting= simpleFunction(object)
+// console.log(greeting)
+
+const {name,...newObject}= object;
+console.log(name);
+console.log(newObject);
+
+const person1={
+    name: 'Omni Knight',
+    class:'Suppport',
+    level: 21
+};
+
+const person2={
+    ...person1,
+    name:'Tide Hunter'
+};
+
+console.log('person1 is', person1)
+console.log('person1 is', person2)
+
+const App={
+    start(){
+        console.log('running')
+    },
+    end(){
+        console.log('stop')
+    }
+}
+App.start();
